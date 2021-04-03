2e60630c699624a60139c376e3fe499f6ab463c6
Khalid Dakak <khalid.dakak8@gmail.com>
2021-04-03 17:20:40 -0500

    Moved textfield into center

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from 'react'
 import AlbumList from './AlbumList'
 import { fetchData } from './api/index'
+import TextField from '@material-ui/core/TextField';
+import { withStyles } from '@material-ui/styles';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+  root: {
+    padding: '100px'
+  },
+  main: {
+    background: '#424242',
+    width: '25ch',
+    textAlign: 'center',
+  }
+});
 
 class App extends React.Component {
   state = {
@@ -13,13 +27,32 @@ class App extends React.Component {
   }
 
   render() {
+    const { classes } = this.props;
     const { data } = this.state
     return (
-      <>
-        <AlbumList data={data} />
+      <Grid container spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: '100vh' }}>
+
+        {/* Search Bar */}
+        <Grid item className={classes.root}>
+          <form className={classes.main} noValidate autoComplete="off">
+            <TextField id="filled-basic" label="Enter Album or Artist" variant="filled" color="secondary" />
+          </form>
+        </Grid>
+
+        {/* Album List */}
+        <Grid item>
+          <AlbumList data={data} />
+        </Grid>
+
+        {/* Webpage Style */}
         <style>{'body { background-color: #121212; }'}</style>
-      </>
+      </Grid >
     )
   }
 }
-export default App
+
+export default withStyles(styles)(App);
